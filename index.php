8c1d7cdd84d9ab261e816f2362ff1df37443f85d
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-10-06 21:32:20 -0300

    chore: superglobals

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    require 'dados.php';
+?>
+
 <!DOCTYPE html>
 <html lang="pt-Br">
 <head>
@@ -35,57 +39,22 @@
         </form>
 
         <section class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-                <div class="flex">
-                    <div class="w-1/3">Imagem</div>
-
-                    <div>
-                        <a href="/livro.php?" class="font-semibold hover:underline">Título</a>
-                        <div class="text-xs italic">Autor</div>
-                        <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliações)</div>
-                    </div>
-                </div>
-                <div class="text-sm">Descrição</div>
-            </div>
+            <?php foreach ($books as $book): ?>
 
             <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
                 <div class="flex">
                     <div class="w-1/3">Imagem</div>
 
-                    <div>
-                        <a href="/livro.php?" class="font-semibold hover:underline">Título</a>
-                        <div class="text-xs italic">Autor</div>
+                    <div class="space-y-1">
+                        <a href="/livro.php?id=<?= $book['id'] ?>" class="font-semibold hover:underline"><?= $book['titulo'] ?></a>
+                        <div class="text-xs italic"><?= $book['autor'] ?></div>
                         <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliações)</div>
                     </div>
                 </div>
-                <div class="text-sm">Descrição</div>
+                <div class="text-sm mt-2"><?= $book['descricao'] ?></div>
             </div>
 
-            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-                <div class="flex">
-                    <div class="w-1/3">Imagem</div>
-
-                    <div>
-                        <a href="/livro.php?" class="font-semibold hover:underline">Título</a>
-                        <div class="text-xs italic">Autor</div>
-                        <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliações)</div>
-                    </div>
-                </div>
-                <div class="text-sm">Descrição</div>
-            </div>
-
-            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-                <div class="flex">
-                    <div class="w-1/3">Imagem</div>
-
-                    <div>
-                        <a href="/livro.php?" class="font-semibold hover:underline">Título</a>
-                        <div class="text-xs italic">Autor</div>
-                        <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliações)</div>
-                    </div>
-                </div>
-                <div class="text-sm">Descrição</div>
-            </div>
+            <?php endforeach; ?>
         </section>
     </main>
 </body>
--- livro.php
+++ livro.php
@@ -1,3 +1,14 @@
+<?php
+
+    require 'dados.php';
+
+    $id = $_REQUEST['id'];
+
+    $filtrado = array_filter($books, fn($book) => $book['id'] == $id);
+
+    $book = array_pop($filtrado);
+?>
+
 <!DOCTYPE html>
 <html lang="pt-Br">
 <head>
@@ -33,7 +44,27 @@
 
 <main class="mx-auto max-w-screen-lg space-y-6">
 
-    //muda aqui
+    <?= $book['titulo']; ?>
+
+    <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+
+        <div class="flex">
+
+            <div class="w-1/3">Imagem</div>
+
+            <div class="space-y-1">
+
+                <a href="/livro.php?id=<?= $book['id'] ?>" class="font-semibold hover:underline"><?= $book['titulo'] ?></a>
+                <div class="text-xs italic"><?= $book['autor'] ?></div>
+                <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliações)</div>
+
+            </div>
+
+        </div>
+
+        <div class="text-sm mt-2"><?= $book['descricao'] ?></div>
+
+    </div>
 
 </main>
 
