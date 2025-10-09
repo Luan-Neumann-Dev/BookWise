d6fa03cd7eac5f1fd15b5c2c4dc5c877797149a5
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-10-09 20:36:38 -0300

    chore: start validations on register

--- controllers/registrar.controller.php
+++ controllers/registrar.controller.php
@@ -1,7 +1,36 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $resultado = $database->query(
+
+    $validations = [];
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $confirmationEmail = $_POST['confirmation_email'];
+    $password = $_POST['password'];
+
+    if (strlen($name) == 0) {
+        $validations[] = 'O nome é obrigatório';
+    }
+    if (strlen($email) == 0) {
+        $validations[] = 'O email é obrigatório';
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $validations[] = 'O email é inválido!';
+    }
+    if ($email != $confirmationEmail) {
+        $validations[] = 'O email de confirmação está diferente';
+    }
+    if (strlen($password) < 8 || strlen($password) > 30) {
+        $validations[] = 'A senha precisa ter entre 8 a 30 caracteres';
+    }
+    if (sizeof($validations) > 0) {
+        $_SESSION['validations'] = $validations;
+        header('Location: /login');
+        exit();
+    }
+
+    $database->query(
         query: "insert into users (name, email, password) values (:name, :email, :password)",
         params: [
             ':name' => $_POST['name'],
