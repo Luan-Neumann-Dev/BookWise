44353256a8b267cff691f581120708d7998adb10
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-10-08 23:23:01 -0300

    feat: layout form login

--- views/template/app.php
+++ views/template/app.php
@@ -19,7 +19,7 @@
             </ul>
 
             <ul>
-                <li><a href="/login.php">Fazer Login</a></li>
+                <li><a href="/login">Fazer Login</a></li>
             </ul>
         </nav>
     </header>
