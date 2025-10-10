b6239d74569e431dc52550ee48406d0f472272ce
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-10-09 21:33:28 -0300

    chore: create user model

--- controllers/login.controller.php
+++ controllers/login.controller.php
@@ -5,14 +5,26 @@ $mensagem = $_REQUEST["mensagem"] ?? '';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_REQUEST["email"];
     $password = $_REQUEST["password"];
+
+    $validation = Validation::validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+    ], $_POST);
+
+    if ($validation->failed()) {
+        header("Location: /login");
+        exit();
+    }
+
     $user = $database->query(
         query: 'select * from users where email = :email and password = :password',
+        class: User::class,
         params: compact("email", "password")
     )->fetch();
 
     if ($user) {
         $_SESSION['auth'] = $user;
-        $_SESSION['mensagem'] = "Seja bem-vindo {$user['name']}!";
+        $_SESSION['mensagem'] = "Seja bem-vindo {$user->name}!";
         header("Location: /");
         exit();
     }
