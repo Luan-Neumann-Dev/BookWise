1bd3d573b747e9fe5df135e557d21660238bb03c
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-10-13 20:18:57 -0300

    chore: encrypt the password

--- controllers/login.controller.php
+++ controllers/login.controller.php
@@ -17,12 +17,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     $user = $database->query(
-        query: 'select * from users where email = :email and password = :password',
+        query: 'select * from users where email = :email',
         class: User::class,
-        params: compact("email", "password")
+        params: compact("email")
     )->fetch();
 
     if ($user) {
+        $password = $_POST["password"];
+        $bdPassword = $user->password;
+        if (!password_verify($password, $bdPassword)) {
+            flash()->push('validations_login', ['Usuário oou senha estão incorretos!']);
+            header("Location: /login");
+            exit();
+        }
+
         $_SESSION['auth'] = $user;
         flash()->push('mensagem', "Seja bem-vindo {$user->name}!");
         header("Location: /");
--- controllers/registrar.controller.php
+++ controllers/registrar.controller.php
@@ -16,9 +16,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $database->query(
         query: "insert into users (name, email, password) values (:name, :email, :password)",
         params: [
-            ':name' => $_POST['name'],
-            ':email' => $_POST['email'],
-            ':password' => $_POST['password']
+            'name' => $_POST['name'],
+            'email' => $_POST['email'],
+            'password' => password_hash($_POST['password'], PASSWORD_DEFAULT)
         ]
     );
 
